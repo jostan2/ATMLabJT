a5bd549ea74fc074f7ee74cbece6e09ad751fd7f
joshua <joshuahsientan@gmail.com>
2022-10-07 15:22:19 -0500

    Add project files.

---
+++ ATM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMLabJT
+{
+    internal class ATM
+    {
+        public List<Account> Accounts{ get; set; }
+        public ATM (List<Account> Accounts)
+        {
+            this.Accounts = Accounts;
+        }
+
+
+        public void AddBalance(string name, int BalanceChange) //function to add or subtract(?) funds from each account
+        {
+            if (Accounts.Where(a => a.Name == name).Count() > 0)
+            {
+                Account Account = Accounts.Where(a => a.Name == name).First();
+                Account.Balance += BalanceChange;
+            }
+            else
+            {
+                Console.WriteLine("No Account Found with that name");
+            }
+        }
+
+        public void AddAccount(string newName, string newPass)
+        {
+            Accounts.Add(new Account(newName, newPass, 0));
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics.Metrics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ATMLabJT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            List<Account> Accounts = new List<Account>()
+            {
+                new Account("Jack","pass1",100),
+                new Account("Bill","pass2", 200),
+                new Account("Fran","pass3", 300),
+                new Account("Aaron","pass4", 400)
+            };
+            ATM atm = new ATM (Accounts);
+
+            //allow user to log in with their own login (username and password)
+
+            //set default "account" in class to default, as the user will need to log in.
+            // in atm class, use a constructor to call the a.account.
+                //have a list that is empty, and add usernames/pass/ect to it as the program runs.
+
+
+
+            //Console.WriteLine("Please enter your username to login (case sensitive)");
+            //string accUser = Console.ReadLine();
+            //if (accUser.Contains( Accounts a.Name ))
+
+
+
+            //allow user to register a new account
+            string newName;
+            string newPass;
+            Console.WriteLine("To create a new account, please state your username:");
+            newName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your password. (case-sensitive)");
+            newPass = Console.ReadLine();
+
+            atm.AddAccount(newName, newPass);
+
+            printAccountList(Accounts);
+
+
+            //allow user (when logged in) to deposit or withdraw money
+
+
+
+        }
+        
+        public static void printAccountList(List<Account> Accounts)
+        {
+            for (int i = 0; i < Accounts.Count; i++)
+            {
+                Account a = Accounts[i];
+                Console.WriteLine($" {a.Name}");
+            }
+        }
+    }
+}
